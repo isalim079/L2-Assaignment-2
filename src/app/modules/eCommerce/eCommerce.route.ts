4f63f49fdd11f5999b489e8abe59926e7dbe88c5
isalim079 <salimreza1025@gmail.com>
2024-05-21 16:26:32 +0600

    eCommerce route for post api created

--- src/app/modules/eCommerce/eCommerce.route.ts
+++ src/app/modules/eCommerce/eCommerce.route.ts
@@ -1,5 +1,6 @@
 import express from 'express'
+import { eCommerceControllers } from './eCommerce.controller'
 
 const router = express.Router()
 
-router.post('/api/products')
+router.post('/api/products', eCommerceControllers.createECommerceData)
